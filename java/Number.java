dffcd520480f6bbcb5587c5c355f3d16a8455e16
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-20 21:03:40 +0900

    Number

---
+++ java/Number.java
@@ -0,0 +1,14 @@
+public class Number {
+
+    public static void main(String[] args) {
+        // Operator
+        System.out.println(1 + 2); // 3
+        System.out.println(2 - 1); // 1
+        System.out.println(1 * 2); // 2
+        System.out.println(2 / 1); // 2
+
+        System.out.println(Math.PI); // 3.14...
+        System.out.println(Math.floor(Math.PI)); // 소숫점 버림
+        System.out.println(Math.ceil(Math.PI)); // 소숫점 올림
+    }
+}
