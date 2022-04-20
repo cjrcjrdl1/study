7300f1ceb0f2d8fb8f2f5d265ccc4b50521dc31d
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-20 21:08:21 +0900

    StringApp

---
+++ java/StringApp.java
@@ -0,0 +1,19 @@
+public class StringApp {
+
+    public static void main(String[] args) {
+
+        // Character VS String
+        System.out.println("Hello World");
+        System.out.println('H');
+        System.out.println("H");
+
+        System.out.println("Hello "
+                + "World");
+
+        // new line
+        System.out.println("Hello \nWorld");
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
