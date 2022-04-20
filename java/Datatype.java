b1ccbfa0778b563ecdfd4c8925aaaa622804cd3e
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-20 20:55:50 +0900

    Datatype

---
+++ java/Datatype.java
@@ -0,0 +1,20 @@
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println(1); // Number
+        System.out.println("six"); // String
+
+        System.out.println("1"); // String 1
+
+        System.out.println(1+1); // 2
+        System.out.println("1" + "1"); // 11
+
+        System.out.println(2*2); // 4
+//        System.out.println("2"*"2");
+
+        System.out.println("Hello World"); // String 문자열
+        System.out.println('H'); // Char 문자
+        System.out.println("H");
+
+        System.out.println("1234".length()); // 4 length는 문자열의 길이
+    }
+}
