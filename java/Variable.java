2493db6433b6851402983d41ac4339e212ce6c7c
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-20 21:17:39 +0900

    Variable

---
+++ java/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+
+    public static void main(String[] args) {
+
+        int a = 1; // Number -> integer ... -1, 0, 1 ...
+        System.out.println(a);
+
+        double b = 1.1; // real number -> double ... -1.0, 0, 1.0 ...
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c);
+
+    }
+}
