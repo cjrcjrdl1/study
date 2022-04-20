6cf5db9407fec1decd6a889323b8d79bad1c8236
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-20 21:18:11 +0900

    Letter

---
+++ java/Letter.java
@@ -0,0 +1,11 @@
+public class Letter {
+
+    public static void main(String[] args) {
+        String name = "min";
+        String hobby = "health";
+        System.out.println("Hello, " + name + " Do you like " + hobby + "?");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
